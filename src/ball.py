7616b815160e99ada2ad3344edbb7b84a4d18d5b
MassKonFuzion <masskonfuzion@masskonfuzion.com>
2016-04-07 19:21:28 -0500

    Updated game to use grid slots. The game scales the objects to fill in the grid slots

--- src/ball.py
+++ src/ball.py
@@ -1,6 +1,27 @@
 from gameobj import GameObj
 import pygame
 
+class BallControlState:
+    def __init__(self):
+        self.leftKeyPressed = False
+        self.rightKeyPressed = False
+
+    def setLeftKeyPressedTrue(self):
+        self.leftKeyPressed = True
+
+    def setLeftKeyPressedFalse(self):
+        self.leftKeyPressed = False
+
+    def setRightKeyPressedTrue(self):
+        self.rightKeyPressed = True
+
+    def setRightKeyPressedFalse(self):
+        self.rightKeyPressed = False
+
+    def reset(self):
+        self.leftKeyPressed = False
+        self.rightKeyPressed = False
+
 class Ball(GameObj):
     def __init__(self):
         super(Ball, self).__init__()
@@ -18,18 +39,32 @@ class Ball(GameObj):
         self._objState = 'falling'
         # TODO: set speed based on state. e.g. When falling, vertical speed is ALWAYS 1 grid slot down the screen per update
 
+        #self._update_delay_dict = { 'on_row' : 
+        #                            { 0 : {'init': 0.01, 'inc': 0, 'max': 0.01, 'dec': 0, 'floor': 0.01}
+        #                            , 1 : {'init': 0, 'inc': 0, 'max': 0, 'dec': 0, 'floor': 0 }
+        #                            }
+        #                          , 'falling' :
+        #                            { 0 : {'init': 0.01, 'inc': 0, 'max': 0.01, 'dec': 0, 'floor': 0.01 }
+        #                            , 1 : {'init': 0.2, 'inc': 0.2, 'max': 0.2, 'dec': 0.001, 'floor': 0.010 }
+        #                            }
+        #                          }
+
+        # NOTE: The dict below is test crap. Delete it once the bugs are worked out
         self._update_delay_dict = { 'on_row' : 
-                                    { 0 : {'init': 0.25, 'inc': 0, 'max': 0.25, 'dec': 0, 'floor': 0.25}
+                                    { 0 : {'init': 0.01, 'inc': 0, 'max': 0.01, 'dec': 0, 'floor': 0.01}
                                     , 1 : {'init': 0, 'inc': 0, 'max': 0, 'dec': 0, 'floor': 0 }
                                     }
                                   , 'falling' :
-                                    { 0 : {'init': 0.25, 'inc': 0, 'max': 0.25, 'dec': 0, 'floor': 0.25 }
-                                    , 1 : {'init': 0.1, 'inc': 0.1, 'max': 0.1, 'dec': 0.005, 'floor': 0.005 }
+                                    { 0 : {'init': 0.01, 'inc': 0, 'max': 0.01, 'dec': 0, 'floor': 0.01 }
+                                    , 1 : {'init': 0.25, 'inc': 0, 'max': 0.25, 'dec': 0, 'floor': 0.25 }
                                     }
                                   }
 
+
         self._update_delay = [ self._update_delay_dict[self._objState][0]['init'], self._update_delay_dict[self._objState][1]['init'] ]
 
+        # Ball Control State
+        self.controlState = BallControlState()
 
     def draw(self, screen, cell_size):
         color = 0,0,255
@@ -40,6 +75,31 @@ class Ball(GameObj):
         
         pygame.draw.rect(screen, color, my_rect)
 
+
+
+    ##def respondToControllerInput(self):
+    ##    """ Set velocity according to user's input
+
+    ##    NOTE:  This function ONLY moves the ball's x/y position.  It does not handle falling due to gravity or
+    ##    collisions.
+    ##    """
+
+    ##    # NOTE TO SELF:  This is not efficient -- to optimize:  only set the x component
+    ##    # if self.direction == -1:
+    ##    #     Vector2D_setxy(self.velocity, -self.maxSpeed, self.velocity[1])
+    ##    # elif self.direction == 1:
+    ##    #     Vector2D_setxy(self.velocity, self.maxSpeed, self.velocity[1])
+    ##    # elif self.direction == 0:
+    ##    #     Vector2D_setxy(self.velocity, 0, self.velocity[1])
+
+    ##    if self.direction == -1:
+    ##        Vector2D_setxy(self.currPhysState.velocity, -self.maxSpeed, self.currPhysState.velocity[1])
+    ##    elif self.direction == 1:
+    ##        Vector2D_setxy(self.currPhysState.velocity, self.maxSpeed, self.currPhysState.velocity[1])
+    ##    elif self.direction == 0:
+    ##        Vector2D_setxy(self.currPhysState.velocity, 0, self.currPhysState.velocity[1])
+
+
     def update(self, dt_s, cell_size):
         #self.accumulator_s += dt_s
 
@@ -49,6 +109,15 @@ class Ball(GameObj):
         #    # Update position
         #    # NOTE: Here's where things get cool. _Every obj has a max speed of exactly 1 grid slot up/down and left/right per frame_. "Speed" is emulated based on update delay, rather than # of spaces moved per update
         #    self._position[1] += 1
+
+		# Prepare velocity and stuff based on user input (NOTE: With better design, this call would take place in a function that reads messages off a message queue)
+        if self.controlState.leftKeyPressed:
+            self._speed[0] = -self._maxSpeed[0]
+        elif self.controlState.rightKeyPressed:
+            self._speed[0] = self._maxSpeed[0]
+
+
+        # Move the ball (if necessary)
 
         for i in xrange(0, len(self._accumulator_s)):
             self._accumulator_s[i] += dt_s
--- src/falldown.py
+++ src/falldown.py
@@ -7,21 +7,28 @@ import sys
 # Import game objects (perhaps this can go into a "game manager" of some sort?)
 from ball import Ball
 
+def drawGrid(screen, cell_size, screen_size):
+    for i in range(0, 63):
+        color = 192, 192, 192
+        pygame.draw.line(screen, color, ( (i + 1) * cell_size[0], 0                      ), ( (i + 1) * cell_size[1], screen_size[1]         ) )
+        pygame.draw.line(screen, color, ( 0                     , (i + 1) * cell_size[0] ), ( screen_size[1]        , (i + 1) * cell_size[1] ) )
+
 def main():
     pygame.init()
 
     # dirt-nasty initialization: screen_size is a tuple (width, height); width, height is initialized as 640x640
     screen_size = width, height = 640, 640
-    cell_size = width / 64, height / 64 # recompute this if screen size changes
     #screen_size = width, height = 854, 640 # The playable area will be a 640 x 640 square.
+    cell_size = width / 64, height / 64 # cell size (starts off as 10px x 10px. recompute this if screen size changes)
     screen = pygame.display.set_mode(screen_size)
 
     bg_col = 255,255,255
 
     # TODO rename ball; add it to a list of game objects. e.g. [ ball, [ rows ] ], or maybe even better: [ ball, row_mgr ] (where row_mgr contains rows)
     a = Ball()
-    a.setPosition(10, 10)
+    a.setPosition(10, 10) # Position is given in coordinates on the 64x64 grid. Actual screen coordinates are calculated from grid coordinates
     a.setSpeed(0,1)
+    a.setMaxSpeed(1,1)
 
     prev_time = pygame.time.get_ticks()
     while True:
@@ -34,7 +41,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+				# NOTE!!!! In a better-designed game, this keydown event would put a message on a message queue, which a destination of "Ball" or whatever. Then, the ball, which is listening to the queue, gets the message
+                ballRef = a     # Set a reference to the ball (NOTE: Later on, we'll want to refer to something else, either an item in a array or some other such thing)
 
+                # Left arrow key
+                if (event.key == pygame.K_LEFT or event.key == pygame.K_j):
+                    ballRef.controlState.setLeftKeyPressedTrue()
+                # Right arrow key
+                elif (event.key == pygame.K_RIGHT or event.key == pygame.K_l):
+                    ballRef.controlState.setRightKeyPressedTrue()
+            elif event.type == pygame.KEYUP:
+                if (event.key == pygame.K_LEFT or event.key == pygame.K_j):
+                    ballRef.controlState.setLeftKeyPressedFalse()
+                elif (event.key == pygame.K_RIGHT or event.key == pygame.K_l):
+                    ballRef.controlState.setRightKeyPressedFalse()
 
         # Update stuff
         a.update(dt_s, cell_size)
@@ -45,17 +66,21 @@ def main():
         for i in range(0, 2):
             if a._position[i] < 0:
                 a._position[i] = 0
-            if a._position[i] > 63:
-                a._position[i] = 63
+            if a._position[i] + a._size[i] > 64:
+                a._position[i] = 64 - a._size[i] # NOTE: Here, we're calculating against 63, not 64, because we're zero-based
 
         # Draw stuff
         screen.fill(bg_col)
+
+        drawGrid(screen, cell_size, screen_size)
         a.draw(screen, cell_size)
 
         # post-render (e.g. score/overlays)
 
         
         pygame.display.flip()
+
+        ## The stuff between here and tne __name__ test is debug stuff. You can delete it once the bugs are worked out
 
 if __name__ == '__main__':
     main()
